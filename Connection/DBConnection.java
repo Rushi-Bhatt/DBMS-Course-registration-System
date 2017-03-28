8ac13cdbe67d2ff3ae1ea7e20c85891d8f5367d0
zndesai <zankrutid24@gmail.com>
2017-03-28 12:40:39 -0400

    master
    master

---
+++ Connection/DBConnection.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+
+
+public class DBConnection {
+
+
+
+    static final String jdbcURL = "jdbc:oracle:thin:@orca.csc.ncsu.edu:1521:orcl01";
+
+
+
+    public static Connection ConnectDB() throws ClassNotFoundException, SQLException {
+
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            
+            String user = "zndesai";	
+            String passwd = "200151915";	
+
+            Connection conn = null;
+            
+            conn = DriverManager.getConnection(jdbcURL, user, passwd);
+            
+            return conn;
+    }
+
+    static void close(Connection conn) {
+        if(conn != null) {
+            try { conn.close(); } catch(Throwable whatever) {}
+        }
+    }
+}
