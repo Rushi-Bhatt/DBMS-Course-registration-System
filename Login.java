d48cfa66c52c07645a188aeb58bd4ba77c979c8f
zndesai <zankrutid24@gmail.com>
2017-03-28 12:06:55 -0400

    master
    master

---
+++ Login.java
@@ -0,0 +1,67 @@
+import java.sql.*;
+import java.util.Scanner;
+
+
+public class Login {
+	public static void main(String[] args) throws Exception
+	{
+		loginmenu();
+	}
+	
+	public static void loginmenu() throws Exception {
+		
+		Connection conn = DBConnection.ConnectDB();
+		Scanner scan = new Scanner(System.in);
+		while(true)
+		{
+			System.out.println("---------Student Registration---------");
+			System.out.println("1. Login");
+			System.out.println("2. Exit");
+	        System.out.println("Enter your choice :-> ");
+			int select = scan.nextInt();
+			if(select == 1)
+			{
+				userCheck(conn);
+			}
+			else
+			{
+				System.exit(0);
+			}
+		}
+	}
+	
+	public static boolean userCheck(Connection conn)throws Exception
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Username");
+		String user = scan.nextLine();
+		System.out.println("Enter Password");
+		String pwd = scan.nextLine();
+		PreparedStatement stmt = conn.prepareStatement("SELECT USERNAME,PASSWORD,ROLE,PERSON_ID FROM USER_LOGIN WHERE USERNAME=?");
+		stmt.setString(1, user);
+		ResultSet rs = stmt.executeQuery();
+		if (!rs.next()) {
+			System.out.printf("User not found. Please Enter correct credentials.\n");
+			return false;
+		}
+		String data_pwd = rs.getString("PASSWORD");
+        String role = rs.getString("ROLE");
+        String username = rs.getString("USERNAME");
+        int personid = rs.getInt("PERSON_ID");
+        if (!pwd.equals(data_pwd)) 
+        {
+            System.out.println("Password Does not match. Please Enter correct credentials");
+            return false;
+        }
+        if(role.equals('A'))
+        {
+        	System.out.println("Welcome! "+username);
+        	AdminHome.adminHome();
+        }
+        else
+        {
+        	System.out.println("Welcome! "+username);
+        }
+        return true;
+	}
+}
